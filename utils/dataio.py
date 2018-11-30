d61818717963dc6281483012ee2662d2de794195
Lisa Morgan <lisakm.morgan@gmail.com>
2018-11-30 16:41:46 +0100

    Swich to pkgutil.get_data for reading the config file.

--- utils/dataio.py
+++ utils/dataio.py
@@ -1,13 +1,11 @@
 import requests
 import json
-import os
 import pkgutil
 
 def get_data(day, year = 2018):
     url = get_url(day, year)
-    __config_location__ = os.path.join(os.path.dirname(__file__),"config.json")
-    with open(__config_location__,"r") as read_file:
-        config = json.load(read_file)
+    config_data = pkgutil.get_data(__package__, 'config.json')
+    config = json.loads(config_data)
     r = requests.get(url, cookies=config["cookie"])
     return r.text
 
