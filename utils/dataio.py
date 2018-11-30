793506a79dbf4e3606e728ec37c5ef0bd7132e81
Lisa Morgan <lisakm.morgan@gmail.com>
2018-11-30 17:22:49 +0100

    change order of functions and fix indentation of return statement.

--- utils/dataio.py
+++ utils/dataio.py
@@ -3,6 +3,17 @@ import json
 import pkgutil
 import os
 
+def load_data(day, year = 2018):
+    filename = ''.join(['data//', str(year), '//day', str(day), '.dat'])
+    if not os.path.exists(filename):
+        data = get_data(day, year)
+        with open( filename , 'wt') as file:
+            file.write(data)
+    else:
+        with open( filename , 'r') as file:
+            data = file.read()
+    return data
+
 def get_data(day, year = 2018):
     url = get_url(day, year)
     config_data = pkgutil.get_data(__package__, 'config.json')
@@ -15,14 +26,3 @@ def get_url(day, year = 2018):
     return '/'.join([
         'https://adventofcode.com', str(year), 'day', str(day), 'input'
         ])
-
-def load_data(day, year = 2018):
-    filename = ''.join(['data//day', str(day), '.dat'])
-    if not os.path.exists(filename):
-        data = get_data(day, year)
-        with open( filename , 'wt') as file:
-            file.write(data)
-    else:
-        with open( filename , 'r') as file:
-            data = file.read()
-        return data
