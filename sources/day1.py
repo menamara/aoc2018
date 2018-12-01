465793d422a717cba4dd55597578eb1afae8ac8a
Lisa Morgan <lisakm.morgan@gmail.com>
2018-12-01 12:19:13 +0100

    Implement the solution for day 1 part 2 including tests.

--- sources/day1.py
+++ sources/day1.py
@@ -7,6 +7,17 @@ def add(input, init_value = 0):
         result += number
     return result
 
+def find_loop(input):
+    buffer = bytearray(1000000)
+    v = memoryview(buffer)
+    result = 0
+    while True:
+        for number in input:
+            if v[result] == 1:
+                return result
+            else:
+                v[result] = 1
+                result += number
 
 if __name__ == '__main__':
     data = dataio.load_data(1)
@@ -14,3 +25,4 @@ if __name__ == '__main__':
     lines = list(filter(None, lines))
     input = list(map(int, lines))
     print(add(input))
+    print(find_loop(input))
--- test/test_days.py
+++ test/test_days.py
@@ -7,3 +7,9 @@ class Test_TestDays(unittest.TestCase):
         self.assertEqual(day1.add([+1, +1, +1]), 3)
         self.assertEqual(day1.add([+1, +1, -2]), 0)
         self.assertEqual(day1.add([-1, -2, -3]), -6)
+                
+    def test_day1_2(self):
+        self.assertEqual(day1.find_loop([+1, -1]), 0)
+        self.assertEqual(day1.find_loop([+3, +3, +4, -2, -4]), 10)
+        self.assertEqual(day1.find_loop([-6, +3, +8, +5, -6]), 5)
+        self.assertEqual(day1.find_loop([+7, +7, -2, -7, -4]), 14)
