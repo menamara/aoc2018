9b55d4b10d3c265e69046c68b0a21a9208f0d6b6
Lisa Morgan <lisakm.morgan@gmail.com>
2018-12-05 07:07:54 +0100

    Create basic structure for day 5

---
+++ day5.py
@@ -0,0 +1,10 @@
+from aoc_helpers import dataio
+
+if __name__ == '__main__':
+    data = dataio.load_data(5)
+    input = dataio.split_data(data,'\n')
+
+    print(' '.join(['The solution for day 4 part 1 = ', 
+                    '']))
+    print(' '.join(['The solution for day 4 part 2 =',
+                    '']))
---
+++ test/test_day5.py
@@ -0,0 +1,9 @@
+import unittest
+import day5
+
+class Test_Day5(unittest.TestCase):
+    def setUp(self):
+        self.test_input = []
+
+    def test_day5(self):
+        self.assertEquals()
