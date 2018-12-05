3136bb7aa98a714cd395d6c8ee80db24976be6bb
Lisa Morgan <lisakm.morgan@gmail.com>
2018-12-05 07:21:29 +0100

    Implement the basic concept and tests for day 5

--- day5.py
+++ day5.py
@@ -1,5 +1,16 @@
 from aoc_helpers import dataio
 
+def reduce(string_input):
+    """ Reduce the input from all adjacent characters that are the same letter and differend case."""
+    return ''
+
+class Polymer:
+    def __init__(self, input_string):
+        self.string = input_string
+
+    def reduced_length(self):
+        return len(reduce(self.string))
+
 if __name__ == '__main__':
     data = dataio.load_data(5)
     input = dataio.split_data(data,'\n')
--- test/test_day5.py
+++ test/test_day5.py
@@ -3,7 +3,15 @@ import day5
 
 class Test_Day5(unittest.TestCase):
     def setUp(self):
-        self.test_input = []
+        test_input = 'dabAcCaCBAcCcaDA'
+        self.polymer = day5.Polymer(test_input)
 
-    def test_day5(self):
-        self.assertEquals()
+    def test_reduction(self):
+        self.assertEqual(day5.reduce('aA'),'')
+        self.assertEqual(day5.reduce('ab'),'')
+        self.assertEqual(day5.reduce('abBA'),'')
+        self.assertEqual(day5.reduce('abAB'),'abAB')
+        self.assertEqual(day5.reduce('aabAAB'),'aabAAB')
+    
+    def test_polymer(self):
+        self.assertEquals(self.polymer.reduced_length(), 10)
