bc8bba1f51ab7cfd19a99c2fde2a5e0566f73375
Lisa Morgan <lisakm.morgan@gmail.com>
2018-12-05 07:59:44 +0100

    Solve day 5 part 1: Reduce polymers

--- day5.py
+++ day5.py
@@ -2,7 +2,19 @@ from aoc_helpers import dataio
 
 def reduce(string_input):
     """ Reduce the input from all adjacent characters that are the same letter and differend case."""
-    return ''
+    str = list(string_input)
+    lentgh = len(string_input)
+    i = 0
+    while i < lentgh-1:
+        if (abs(ord(str[i])-ord(str[i+1])) == 32):
+            str[i:i+2] = []
+            lentgh -= 2
+            i -= 1
+        else:
+            i += 1
+        
+    return ''.join(str)
+
 
 class Polymer:
     def __init__(self, input_string):
@@ -14,8 +26,9 @@ class Polymer:
 if __name__ == '__main__':
     data = dataio.load_data(5)
     input = dataio.split_data(data,'\n')
+    polymer = Polymer(input[0])
 
     print(' '.join(['The solution for day 4 part 1 = ', 
-                    '']))
+                    str(polymer.reduced_length())]))
     print(' '.join(['The solution for day 4 part 2 =',
                     '']))
--- test/test_day5.py
+++ test/test_day5.py
@@ -7,11 +7,15 @@ class Test_Day5(unittest.TestCase):
         self.polymer = day5.Polymer(test_input)
 
     def test_reduction(self):
+        self.assertEqual(day5.reduce('a'),'a')
         self.assertEqual(day5.reduce('aA'),'')
-        self.assertEqual(day5.reduce('ab'),'')
+        self.assertEqual(day5.reduce('aAa'),'a')
+        self.assertEqual(day5.reduce('ab'),'ab')
         self.assertEqual(day5.reduce('abBA'),'')
         self.assertEqual(day5.reduce('abAB'),'abAB')
         self.assertEqual(day5.reduce('aabAAB'),'aabAAB')
+        self.assertEqual(day5.reduce('aabAAB'),'aabAAB')
+        self.assertEqual(day5.reduce('dabAcCaBCBAcCcaDA'),'daCBAcaDA')
     
     def test_polymer(self):
         self.assertEquals(self.polymer.reduced_length(), 10)
