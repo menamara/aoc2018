cac52ee1b429d884e30d3c1552bae5e2c49f79df
Lisa Morgan <lisakm.morgan@gmail.com>
2018-12-21 22:16:16 +0100

    Put metadata counting in a class

--- day8.py
+++ day8.py
@@ -1,32 +1,34 @@
-test_data = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
-idx = 0
-md_sum = 0        
+from aoc_helpers import dataio
 
-def process_node():
-    global idx, test_data, md_sum
-    n_mdata = test_data[idx+1]
-    if test_data[idx] == 0:
-        md_sum += sum(test_data[idx+2:idx+2+n_mdata])
-        del test_data[idx:idx+2+n_mdata]
-        idx -= 2
-        if idx < 0: 
-            print(str(md_sum))
-            return
-        test_data[idx] -= 1
-    else:
-        idx += 2
-    process_node()
+class Licence:
+    def __init__(self, data):
+        self.data = data
+        self.idx = 0
+        self.md_sum = 0        
+
+    def process_node(self):
+        n_mdata = self.data[self.idx+1]
+        if self.data[self.idx] == 0:
+            self.md_sum += sum(self.data[self.idx+2:self.idx+2+n_mdata])
+            del self.data[self.idx:self.idx+2+n_mdata]
+            self.idx -= 2
+            if self.idx < 0: 
+                print(str(self.md_sum))
+                return self.md_sum
+            self.data[self.idx] -= 1
+        else:
+            self.idx += 2
+        return self.process_node()
         
 
 if __name__ == '__main__':
-    #data = dataio.load_data(8)
-    #input = int(dataio.split_data(data,'\n'))
-
-    process_node()
-
+    data = dataio.load_data(8)
+    input = int(dataio.split_data(data,'\n'))
+    licence = Licence(input)
+    
 
     print(' '.join(['The solution for day 8 part 1 =', 
-                   '']))
+                   str(licence.process_node())]))
     print(' '.join(['The solution for day 8 part 2 =',
                    '']))
 
